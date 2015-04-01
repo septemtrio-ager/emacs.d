d00686f38aaa8d65b42530ea93e4797d2be4eb28
Shunsuke KITADA <septemtrio-ager@gmail.com>
2015-04-01 17:57:48 +0900

    el-get-bundleを追加

--- inits/33_markdown-mode.el
+++ inits/33_markdown-mode.el
@@ -9,15 +9,18 @@
 
 ;; ===================================================================
 
-(autoload 'markdown-mode "markdown-mode"
-   "Major mode for editing Markdown files" t)
-(add-to-list 'auto-mode-alist '("\\.text\\'" . gfm-mode))
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . gfm-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
+(el-get-bundle markdown-mode
+  
+  (autoload 'markdown-mode "markdown-mode"
+    "Major mode for editing Markdown files" t)
+  (add-to-list 'auto-mode-alist '("\\.text\\'" . gfm-mode))
+  (add-to-list 'auto-mode-alist '("\\.markdown\\'" . gfm-mode))
+  (add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
 
-;; ファイルパスを渡す
-(defun markdown-custom ()
-  "markdown-mode-hook"
-  (setq markdown-command-needs-filename t)
+  ;; ファイルパスを渡す
+  (defun markdown-custom ()
+    "markdown-mode-hook"
+    (setq markdown-command-needs-filename t)
+    )
+  (add-hook 'markdown-mode-hook '(lambda() (markdown-custom)))
   )
-(add-hook 'markdown-mode-hook '(lambda() (markdown-custom)))
