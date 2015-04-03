8ff569b0b1e49a01aa473ff912b3cf0050ad6c02
Shunsuke KITADA <septemtrio-ager@gmail.com>
2015-04-03 17:32:03 +0900

    auto-completeを起動するようにした

--- inits/36_arduino-mode.el
+++ inits/36_arduino-mode.el
@@ -36,4 +36,10 @@
   
   (setq auto-mode-alist (cons '("\\.\\(pde\\|ino\\)$" . arduino-mode) auto-mode-alist))
   (autoload 'arduino-mode "arduino-mode" "Arduino editing mode." t)
+
+  (add-hook 'arduino-mode-hook
+	    '(lambda ()
+	    (auto-complete-mode t)
+	    ))
+  
   )
