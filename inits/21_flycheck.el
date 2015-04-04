dea72ab85a2e8a39ff0f364efd036d54964fc3da
Shunsuke KITADA <septemtrio-ager@gmail.com>
2015-04-05 01:13:29 +0900

    el-get-bundleでelpa指定をなくした

--- inits/21_flycheck.el
+++ inits/21_flycheck.el
@@ -10,7 +10,7 @@
 ;; ===================================================================
 
 (el-get-bundle flycheck
-  
+
   (require 'flycheck)
   (add-hook 'after-init-hook #'global-flycheck-mode)
 
@@ -18,12 +18,11 @@
   ;;          (lambda () (flycheck-mode t)))
   ;;(add-hook 'java-mode-hook
   ;;          (lambda () (flycheck-mode t)))
-)
+  )
 
-  ;; flycheck-pos-tipを使う
-(el-get-bundle elpa:flycheck-pos-tip
-  :depends (flycheck popup)
-  
+;; flycheck-pos-tipを使う
+(el-get-bundle flycheck-pos-tip
+
   (eval-after-load 'flycheck
     '(custom-set-variables
       '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
