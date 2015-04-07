9a0ec9750495788eb68772009d07adfcd4817c81
Shunsuke KITADA <septemtrio-ager@gmail.com>
2015-04-07 12:13:34 +0900

    magit最新版へ対応。警告メッセージを表示させないようにした

--- inits/37_magit.el
+++ inits/37_magit.el
@@ -27,10 +27,12 @@
   
   (require 'magit)
 
+  (setq magit-last-seen-setup-instructions "1.4.0")
+  
   (eval-after-load 'magit
-  '(progn
-     (set-face-foreground 'magit-diff-add "green3")
-     (set-face-foreground 'magit-diff-del "red3")
-     (when (not window-system)
-       (set-face-background 'magit-item-highlight "white"))))  
+    '(progn
+       (set-face-foreground 'magit-diff-add "green3")
+       (set-face-foreground 'magit-diff-del "red3")
+       (when (not window-system)
+	 (set-face-background 'magit-item-highlight "white"))))  
   )
