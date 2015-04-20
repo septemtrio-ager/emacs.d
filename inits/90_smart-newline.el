556a44f4512e186198dbd9ad25a1bf4ecdbe088b
Shunsuke KITADA <septemtrio-ager@gmail.com>
2015-04-20 13:01:10 +0900

    読み込み順番を変更。hookする系の設定ファイルの読み込み順番を同順位にした

---
+++ inits/90_smart-newline.el
@@ -0,0 +1,43 @@
+;;
+;;; smart-newline.elの設定
+;;
+
+;; ===================================================================
+
+;; 【参考】smart-newline.elという拡張
+;; http://ainame.hateblo.jp/entry/2013/12/08/162032
+
+;; ===================================================================
+
+(el-get-bundle smart-newline
+  
+  ;; c-modeで利用する
+  (add-hook 'c-mode-hook 'smart-newline-mode)
+
+  ;; c++-modeで利用する
+  (add-hook 'c++-mode-hook 'smart-newline-mode)
+
+  ;; java-modeで利用する
+  (add-hook 'java-mode-hook 'smart-newline-mode)
+
+  ;; python-modeで利用する
+  (add-hook 'python-mode-hook 'smart-newline-mode)
+
+  ;; emacs-lisp-modeで利用する
+  (add-hook 'emacs-lisp-mode-hook 'smart-newline-mode)
+
+  ;; web-modeで利用する
+  (add-hook 'web-mode-hook 'smart-newline-mode)
+
+  ;; css-modeで利用する
+  (add-hook 'css-mode-hook 'smart-newline-mode)
+
+  ;; arduino-modeで利用する
+  (add-hook 'arduino-mode-hook 'smart-newline-mode)
+
+  ;; GFM-modeで利用する
+  (add-hook 'gfm-mode-hook 'smart-newline-mode)
+
+  ;; shell-script-modeで利用する
+  (add-hook 'sh-mode-hook 'smart-newline-mode)
+  )
