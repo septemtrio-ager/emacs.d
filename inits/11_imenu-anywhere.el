fd839ecd33adb56e38d126999382152f983abcc7
Shunsuke KITADA <septemtrio-ager@gmail.com>
2015-04-29 23:12:37 +0900

    imenu-anywhreを導入

---
+++ inits/11_imenu-anywhere.el
@@ -0,0 +1,17 @@
+;;
+;;; imenu-anywhere.elの設定
+;;
+
+;; ===================================================================
+
+;; 【参考】簡易関数定義ジャンプimenuをido/helm化した上で
+;;                                          他のファイルまで拡張する！
+;; http://rubikitch.com/2014/12/17/imenu-anywhere/
+
+;; ===================================================================
+
+(el-get-bundle imenu-anywhre
+
+  (global-set-key (kbd "C-.") 'helm-imenu-anywhere)
+  
+  )
