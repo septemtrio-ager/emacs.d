cda87fce81520523a8ec849392f2350a6eeb594d
Shunsuke KITADA <septemtrio-ager@gmail.com>
2015-05-06 23:38:14 +0900

    hookの設定を変更

--- inits/26_python-mode.el
+++ inits/26_python-mode.el
@@ -23,7 +23,7 @@
 (el-get-bundle py-autopep8
   
   (require 'py-autopep8)
-  (add-hook 'before-save-hook 'py-autopep8-before-save)
+  (add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
   )
 
 ;; Pythonのオートコンプリートをする
