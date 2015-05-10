ec9aa4fe70a67c8d3259899006b5cbd30a2b478f
Shunsuke KITADA <septemtrio-ager@gmail.com>
2015-05-10 22:50:35 +0900

    mwimを追加

---
+++ inits/91_mwim.el
@@ -0,0 +1,17 @@
+;;
+;;; mwim.elの設定
+;;
+
+;; ===================================================================
+
+;; 【参考】コードの先頭・末尾を認識したC-a/C-eを定義する
+;; http://rubikitch.com/2015/01/26/mwim/
+
+;; ===================================================================
+
+(el-get-bundle mwim
+
+  (global-set-key (kbd "C-a") 'mwim-beginning-of-code-or-line)
+  (global-set-key (kbd "C-e") 'mwim-end-of-code-or-line)
+  
+  )
