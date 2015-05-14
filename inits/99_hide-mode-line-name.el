04718008d3e90bcea6fab309e0cb58ff6997bf16
Shunsuke KITADA <septemtrio.ager@gmail.com>
2015-05-14 23:45:52 +0900

    マイナーモードの表示を消した

---
+++ inits/99_hide-mode-line-name.el
@@ -0,0 +1,28 @@
+;;
+;;; 指定したマイナーモードを表示しないようにする
+;;
+
+;; =========================================================
+
+;; 【参考】指定したマイナーモードを表示しない
+;; http://qiita.com/tadsan/items/8b5976682b955788c262
+
+;; ==========================================================
+
+(setq my/hidden-minor-modes
+      ;; 表示させないマイナーモード
+      '(undo-tree-mode
+	helm-mode
+	auto-complete-mode
+	smooth-scroll-mode
+	magit-auto-revert-mode
+	rainbow-mode
+	auto-highlight-symbol-mode
+	smart-newline-mode
+	volatile-highlights-mode
+	))
+
+(mapc (lambda (mode)
+	(setq minor-mode-alist
+	      (cons (list mode "") (assq-delete-all mode minor-mode-alist))))
+      my/hidden-minor-modes)
