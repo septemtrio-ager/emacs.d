dafde4b73bceee0590d6a164c0968c3a1d288d3b
Shunsuke KITADA <septemtrio.ager@gmail.com>
2015-05-17 00:51:12 +0900

    いくつかの設定を追加

--- inits/83_recentf-ext.el
+++ inits/83_recentf-ext.el
@@ -18,6 +18,35 @@
   (setq recentf-auto-save-timer (run-with-idle-timer 30 t 'recentf-save-list))
   (recentf-mode 1)
 
+  
+  ;; =================================================================
+
+  ;;【参考】recentf のホームディレクトリを "~" に置換
+  ;; http://yak-shaver.blogspot.jp/2013/07/recentf.html
+
+  ;; =================================================================
+  
+  (defadvice recentf-open-files (before recentf-abbrev-file-name-adv activate)
+    (recentf-cleanup)
+    (let ((directory-abbrev-alist `((,(concat "\\`" (getenv "HOME")) . "~"))))
+      (setq recentf-list (mapcar #'(lambda (x) (abbreviate-file-name x)) recentf-list))))
+
+  ;; ================================================================
+
+  ;; 【参考】recentf-ext のディレクトリを色付け
+  ;; http://yak-shaver.blogspot.jp/2013/07/recentf-ext.html
+
+  ;; ===============================================================
+  
+  (defadvice recentf-open-files (after recentf-set-overlay-directory-adv activate)
+    (set-buffer "*Open Recent*")
+    (save-excursion
+      (while (re-search-forward "\\(^  \\[[0-9]\\] \\|^  \\)\\(.*/\\)$" nil t nil)
+	(overlay-put (make-overlay (match-beginning 2) (match-end 2))
+		     'face `((:foreground ,"#F1266F"))))))
+
+  
+  
   (add-hook 'after-init-hook
 	    (lambda()
 	      (recentf-open-files)
