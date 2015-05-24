019b1efd3b2e0b59b56acc5b4ab05772e8beb640
Shunsuke KITADA <septemtrio.ager@gmail.com>
2015-05-24 19:24:38 +0900

    閉じ括弧補完を変更した

--- inits/20_basic_programing_setting.el
+++ inits/20_basic_programing_setting.el
@@ -11,20 +11,20 @@
 
 
 
-;;; 閉じ括弧を自動で補完する
-(electric-pair-mode 1)
+;; 閉じ括弧を自動で補完する
+;; (electric-pair-mode 1)
 
 
 
-;;; カーソルの位置が何行目かを表示する
+;; カーソルの位置が何行目かを表示する
 (line-number-mode t)
 
 
 
-;;; 空白を一度に削除する
+;; 空白を一度に削除する
 (setq c-hungry-delete-key t)
 
 
 
-;;; 現在の関数名をモードラインに表示
+;; 現在の関数名をモードラインに表示
 ;; (which-function-mode 1)
