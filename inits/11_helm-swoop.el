6424f99f673e1465f234cff2736de788b3830cae
Shunsuke KITADA <septemtrio.ager@gmail.com>
2015-05-24 19:23:40 +0900

    キーバインドを割り振った

--- inits/11_helm-swoop.el
+++ inits/11_helm-swoop.el
@@ -27,5 +27,16 @@
     (defadvice helm-resume (around helm-swoop-resume activate)
       "helm-anything-resumeで復元できないのでその場合に限定して無効化"
       ad-do-it))
+
+  (defun isearch-forward-or-helm-swoop (use-helm-swoop)
+    (interactive "p")
+    (let (current-prefix-arg
+	  (helm-swoop-pre-input-function 'ignore))
+      (call-interactively
+       (case use-helm-swoop
+	 (1 'isearch-forward)
+	 (4 'helm-swoop)
+	 (16 'helm-swoop-nomigemo)))))
+  (global-set-key (kbd "C-s") 'isearch-forward-or-helm-swoop)
   
   )
