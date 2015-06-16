0f76c28915479e1b5472b94e47a318315d1b9c93
Shunsuke KITADA <septemtrio.ager@gmail.com>
2015-06-17 08:44:26 +0900

    キーバインドを変更

--- inits/99_ac-mozc.el
+++ inits/99_ac-mozc.el
@@ -1,10 +1,10 @@
 ;;
-;;;
+;;; ac-mozc.elの設定
 ;;
 
 (el-get-bundle ac-mozc
 
   (require 'ac-mozc)
-  (define-key ac-mode-map (kbd "C-t") 'ac-complete-mozc)
+  (define-key ac-mode-map (kbd "C-j") 'ac-complete-mozc)
   
   )
