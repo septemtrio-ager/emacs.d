a4ecf4531133d38495c9a37399a9abc6be39fe8b
Shunsuke KITADA <septemtrio.ager@gmail.com>
2015-07-07 13:49:17 +0900

    説明を追加

--- inits/92_volatile-highlights.el
+++ inits/92_volatile-highlights.el
@@ -1,6 +1,13 @@
 ;;
-;;;
+;;; volatile-highlights
 ;;
+
+;; ===================================================================
+
+;; 操作に視覚的フィードバックを与える
+;; https://github.com/k-talo/volatile-highlights.el/blob/master/README-ja.org
+
+;; ===================================================================
 
 (el-get-bundle volatile-highlights
 
