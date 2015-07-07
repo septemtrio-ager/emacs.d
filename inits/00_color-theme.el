edeea4adcfe0adb6f654635d6e9a09a57bb91e9a
Shunsuke KITADA <septemtrio.ager@gmail.com>
2015-07-07 13:51:00 +0900

    テーマを設定するコードをまとめた

---
+++ inits/00_color-theme.el
@@ -0,0 +1,17 @@
+;;
+;;; color themeの設定
+;;
+
+(el-get-bundle monokai-theme
+
+  ;; tango-darkテーマ(デフォルト)
+  (load-theme 'tango-dark t)
+  
+  ;; monokaiテーマ
+  ;; (load-theme 'monokai t)
+  
+  
+  ;; カーソルの色を変更する
+  (set-cursor-color "yellow")
+  
+  )
