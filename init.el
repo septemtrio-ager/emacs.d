1cccf13aa194dc5611cde2a6e73ecdd473ae1605
Shunsuke KITADA <septemtrio.ager@gmail.com>
2015-07-10 18:18:49 +0900

    el-get-lockをinit.elに記述した

--- init.el
+++ init.el
@@ -35,6 +35,12 @@
 
 
 
+;; el-get-lockを用いてバージョン固定したElispをダウンロードする
+(el-get-bundle tarao/el-get-lock)
+(el-get-lock)
+
+
+
 ;; init-loader.elで設定ファイルを分割する
 (el-get-bundle init-loader
 
--- inits/99_el-get-lock.el
+++
@@ -1,6 +0,0 @@
-;;
-;;; el-get-lock.elの設定
-;;
-
-(el-get-bundle tarao/el-get-lock)
-(el-get-lock)
