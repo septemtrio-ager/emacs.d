9de4b43818c1031d5b886a4bfaa21b453c9094cf
Shunsuke KITADA <septemtrio.ager@gmail.com>
2015-07-19 22:44:54 +0900

    electric-operatorを使うようにした

--- inits/35_electric-spacing.el
+++ inits/35_electric-spacing.el
@@ -10,23 +10,23 @@
 
 ;; ===================================================================
 
-(el-get-bundle electric-spacing
+;; (el-get-bundle electric-spacing
   
-  (require 'electric-spacing)
+;;   (require 'electric-spacing)
 
-  ;; c-modeで利用する
-  (add-hook 'c-mode-hook #'electric-spacing-mode)
+;;   ;; c-modeで利用する
+;;   (add-hook 'c-mode-hook #'electric-spacing-mode)
 
-  ;; c++-modeで利用する
-  (add-hook 'c++-mode-hook #'electric-spacing-mode)
+;;   ;; c++-modeで利用する
+;;   (add-hook 'c++-mode-hook #'electric-spacing-mode)
 
-  ;; java-modeで利用する
-  (add-hook 'java-mode-hook #'electric-spacing-mode)
+;;   ;; java-modeで利用する
+;;   (add-hook 'java-mode-hook #'electric-spacing-mode)
 
-  ;; python-modeで利用する
-  (add-hook 'python-mode-hook #'electric-spacing-mode)
+;;   ;; python-modeで利用する
+;;   (add-hook 'python-mode-hook #'electric-spacing-mode)
   
-  ;; arduino-modeで利用する
-  ;; 使いたかったけど対応してなかった
- (add-hook 'arduino-mode-hook #'electric-spacing-mode)
-  )
+;;   ;; arduino-modeで利用する
+;;   ;; 使いたかったけど対応してなかった
+;;  (add-hook 'arduino-mode-hook #'electric-spacing-mode)
+;;   )
