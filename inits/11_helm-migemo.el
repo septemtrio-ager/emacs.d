9c9a11ca83bdd342b9245045c6023d63947636a2
Shunsuke KITADA <septemtrio.ager@gmail.com>
2015-09-07 21:00:59 +0900

    helm-migemoの修正が必要なかったので変更を戻した

--- inits/11_helm-migemo.el
+++ inits/11_helm-migemo.el
@@ -13,29 +13,17 @@
 
   (require 'helm-migemo)
 
-  ;; この修正が必要
-  (with-eval-after-load "helm-migemo"
-    (defun helm-compile-source--candidates-in-buffer (source)
-      (helm-aif (assoc 'candidates-in-buffer source)
-	  (append source
-		  `((candidates
-		     . ,(or (cdr it)
-			    (lambda ()
-			      ;; Do not use `source' because other plugins
-			      ;; (such as helm-migemo) may change it
-			      (helm-candidates-in-buffer (helm-get-current-source)))))
-		    (volatile) (match identity)))
-	source))
-    
-    ;; ===============================================================
-
-    ;; helm-migemoが動かなくなったので対処しました
-    ;; http://rubikitch.com/2015/09/07/update-2/
-
-    ;; ===============================================================
-    
-    ;; [2015-09-06 Sun]helm-match-plugin -> helm-multi-match変更の煽りを受けて
-    (defalias 'helm-mp-3-get-patterns 'helm-mm-3-get-patterns)
-    (defalias 'helm-mp-3-search-base 'helm-mm-3-search-base))
+  (eval-after-load "helm-migemo"
+    '(defun helm-compile-source--candidates-in-buffer (source)
+       (helm-aif (assoc 'candidates-in-buffer source)
+	   (append source
+		   `((candidates
+		      . ,(or (cdr it)
+			     (lambda ()
+			       ;; Do not use `source' because other plugins
+			       ;; (such as helm-migemo) may change it
+			       (helm-candidates-in-buffer (helm-get-current-source)))))
+		     (volatile) (match identity)))
+	 source)))
   
   )
