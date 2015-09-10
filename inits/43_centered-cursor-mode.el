26540fbb3ca16999e8c888dd56207d58500d8ab5
Shunsuke KITADA <septemtrio.ager@gmail.com>
2015-09-10 12:12:11 +0900

    コメントを追加

--- inits/43_centered-cursor-mode.el
+++ inits/43_centered-cursor-mode.el
@@ -2,9 +2,18 @@
 ;;; centered-cursor-mode.elの設定
 ;;
 
+;; ===================================================================
+
+;; 【参考】カーソル移動・スクロール時に常に画面中央行(任意可)を
+;;                                            キープして表示させる
+;; http://rubikitch.com/2015/04/23/centered-cursor-mode/
+
+;; ===================================================================
+
 (el-get-bundle centered-cursor-mode
 
   (require 'centered-cursor-mode)
   (global-centered-cursor-mode 1)
   
   )
+
