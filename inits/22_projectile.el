204afa95e53aa5060554405c22950444fd4e9df8
Shunsuke KITADA <septemtrio.ager@gmail.com>
2015-09-27 00:34:53 +0900

    使わないようにした

--- inits/22_projectile.el
+++ inits/22_projectile.el
@@ -4,8 +4,8 @@
 
 (el-get-bundle projectile
 
-  (projectile-global-mode)
-  (setq projectile-completion-system 'helm)
-  (helm-projectile-on)
+  ;; (projectile-global-mode)
+  ;; (setq projectile-completion-system 'helm)
+  ;; (helm-projectile-on)
   
   )
