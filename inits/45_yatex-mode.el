b6bd19f115199b04af554026695a5837970740be
Shunsuke KITADA <septemtrio.ager@gmail.com>
2015-09-29 01:14:49 +0900

    elpa指定でel-getからインストールするようにした

--- inits/45_yatex-mode.el
+++ inits/45_yatex-mode.el
@@ -9,28 +9,34 @@
 
 ;; =========================================================
 
-(el-get-bundle yatex
+;; =========================================================
 
-  (autoload 'yatex-mode "yatex" "Yet Another LaTeX mode" t)
-  
-  (setq auto-mode-alist
-	(append '(("\\.tex$" . yatex-mode)
-		  ("\\.ltx$" . yatex-mode)
-		  ("\\.cls$" . yatex-mode)
-		  ("\\.sty$" . yatex-mode)
-		  ("\\.clo$" . yatex-mode)
-		  ("\\.bbl$" . yatex-mode)) auto-mode-alist))
-  (setq YaTeX-kanji-code nil)
-  
-  (setq tex-command "platex")
-  
-  (setq dvi2-command "xdvi")
-  
-  (setq bibtex-command "pbibtex")
-  
-  (setq dviprint-command-format "dvipdfmx")
-  
-  ;; ;; 自動で改行しないようにする
-  (add-hook 'yatex-mode-hook '(lambda () (auto-fill-mode -1)))
-  
-  )
+;; 【参考】TeXLive2013 +Emacs + YaTex の設定をした
+;; http://tnil.hatenadiary.jp/entry/20130823/1377230350
+
+;; =========================================================
+
+(el-get-bundle elpa:yatex)
+
+(autoload 'yatex-mode "yatex" "Yet Another LaTeX mode" t)
+
+(setq auto-mode-alist
+      (append '(("\\.tex$" . yatex-mode)
+		("\\.ltx$" . yatex-mode)
+		("\\.cls$" . yatex-mode)
+		("\\.sty$" . yatex-mode)
+		("\\.clo$" . yatex-mode)
+		("\\.bbl$" . yatex-mode)) auto-mode-alist))
+
+(setq YaTeX-kanji-code nil)
+
+(setq tex-command "platex")
+
+(setq dvi2-command "xdvi")
+
+(setq bibtex-command "pbibtex")
+
+(setq dviprint-command-format "dvipdfmx")
+
+;; ;; 自動で改行しないようにする
+(add-hook 'yatex-mode-hook '(lambda () (auto-fill-mode -1)))
