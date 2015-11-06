5872e07753d9d783cbaf710c4726a97e00520bb6
Shunsuke KITADA <septemtrio.ager@gmail.com>
2015-11-06 12:35:49 +0900

    trampを利用するようにした

---
+++ inits/06_tramp.el
@@ -0,0 +1,15 @@
+;;
+;;; tramp.elの設定
+;;
+
+;; ===================================================================
+
+;; 【参考】trampでリモートサーバのファイルをSSH経由で編集
+;; http://qiita.com/muyuu/items/63a82c819bbbe65d37a2
+
+;; ===================================================================
+
+(require 'tramp)
+(setq tramp-default-method "ssh")
+;; (setq tramp-default-method "plink")
+
