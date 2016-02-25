3c037214a5649fe5e5f8d153b834c47510daa484
Shunsuke KITADA <septemtrio.ager@gmail.com>
2016-02-25 21:27:04 +0900

    キーバインドを変更
    mc/mark-more-like-this-extendedを"C-c m e"
    mc/mark-all-like-this-dwimを"C-c m d"に設定

--- inits/44_multiple-cursors.el
+++ inits/44_multiple-cursors.el
@@ -20,27 +20,6 @@
 
 (use-package multiple-cursors
   :defer t
-  
-  :commands (mc/edit-lines-or-string-rectangle mc/mark-all-dwim-or-mark-sexp)
-  
-  :bind (("C-x r t" . mc/edit-lines-or-string-rectangle)
-	 ("C-C-SPC" . mc/mark-all-dwim-or-mark-sexp))
-  
-  :config
-  
-  (defun mc/edit-lines-or-string-rectangle (s e)
-    "C-x r tで同じ桁の場合にmc/edit-lines (C-u M-x mc/mark-all-dwim)"
-    (interactive "r")
-    (if (eq (save-excursion (goto-char s) (current-column))
-	    (save-excursion (goto-char e) (current-column)))
-	(call-interactively 'mc/edit-lines)
-      (call-interactively 'string-rectangle)))
-  
-  (defun mc/mark-all-dwim-or-mark-sexp (arg)
-    "C-u C-M-SPCでmc/mark-all-dwim, C-u C-u C-M-SPCでC-u M-x mc/mark-all-dwim"
-    (interactive "p")
-    (cl-case arg
-      (16 (mc/mark-all-dwim t))
-      (4 (mc/mark-all-dwim nil))
-      (1 (mark-sexp 1)))))
-
+  :bind (("C-c m e" . mc/mark-more-like-this-extended)
+	 ("C-c m d" . mc/mark-all-like-this-dwim))
+  )
