0a08ffdcb0c2ed6f1d00a4b872c817c062c88f8b
septemtrio-ager <septemtrio.ager@gmail.com>
2016-02-29 00:15:56 +0900

    unstageなファイルが存在する場合再びmagit-statusを開くようにした

--- inits/24_magit.el
+++ inits/24_magit.el
@@ -26,5 +26,40 @@
 (el-get-bundle magit)
 
 (use-package magit
-  :defer t
-  :bind ("C-x m" . magit-status))
+  ;; :defer t
+  
+  :bind ("C-x m" . magit-status)
+  
+  :config
+  
+  ;; ステージしてないファイルが存在する場合は再びmagit-statusバッファを開くようにする
+  (defadvice with-editor-finish (around Y/go-back-to-magit-status activate)
+    "Go back ‘magit-status’ if there are other un-staged things."
+    (let ((dir default-directory))
+      ad-do-it
+      (when (magit-anything-unstaged-p)
+	(magit-status-internal dir))))
+  
+  ;; ウィンドウ分割させないようにする
+  ;; (setq magit-display-buffer-function
+  ;; 	(lambda (buffer)
+  ;; 	  (display-buffer
+  ;; 	   buffer (if (and (derived-mode-p 'magit-mode)
+  ;; 			   (memq (with-current-buffer buffer major-mode)
+  ;; 				 '(magit-process-mode
+  ;; 				   magit-revision-mode
+  ;; 				   magit-diff-mode
+  ;; 				   magit-stash-mode
+  ;; 				   magit-status-mode)))
+  ;; 		      nil
+  ;; 		    '(display-buffer-same-window)))))
+  
+  ;; ウィンドウを分割させないようにする
+  ;; 2016-02-29現在で利用しているmagitのバージョンならこっちで対応可能
+  ;; (setq magit-status-buffer-switch-function 'switch-to-buffer)
+  
+  ;; magit-commit時にdiffが開かないようにする
+  ;; C-c C-dすればdiffを見ることができる
+  (remove-hook 'server-switch-hook 'magit-commit-diff)
+  
+  )
