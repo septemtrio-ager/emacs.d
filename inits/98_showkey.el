e75254c887489b5ce3e60b259fcd3465dca76610
Shunsuke KITADA <septemtrio.ager@gmail.com>
2016-04-04 22:47:39 +0900

    showkey.elを追加

---
+++ inits/98_showkey.el
@@ -0,0 +1,17 @@
+;;
+;;; showkey.elの設定
+;;
+
+;; ===================================================================
+
+;; 【参考】キー操作とコマンドの履歴を表示する
+;; http://rubikitch.com/tag/m-x-showkey-log-mode/
+
+;; ===================================================================
+
+(el-get-bundle showkey)
+
+(use-package showkey
+  :defer t
+  :config
+  (push '(font . "Ricty-13") showkey-log-frame-alist))
