013cb851e8bc26b4d42641eed644897a43c20be0
Shunsuke KITADA <septemtrio.ager@gmail.com>
2016-04-11 11:19:42 +0900

    load-pathを追加

--- init.el
+++ init.el
@@ -12,7 +12,7 @@
 	(if (fboundp 'normal-top-level-add-subdirs-to-load-path)
 	    (normal-top-level-add-subdirs-to-load-path))))))
 
-(add-to-load-path "site-lisp" "elpa" "auto-install")
+(add-to-load-path "site-lisp" "elpa" "auto-install" "my-lisp")
 
 
 
