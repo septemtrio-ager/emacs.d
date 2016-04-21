cd8357c524f75d11d7547c50abb4a28b963689d6
Shunsuke KITADA <septemtrio.ager@gmail.com>
2016-04-21 09:54:05 +0900

    my-lispディレクトリにパスを通した

--- init.el
+++ init.el
@@ -12,6 +12,8 @@
 	(if (fboundp 'normal-top-level-add-subdirs-to-load-path)
 	    (normal-top-level-add-subdirs-to-load-path))))))
 
+;; site-lisp, elpa, auto-complete, my-lispディレクトリに
+;; load-pathを通すようにする
 (add-to-load-path "site-lisp" "elpa" "auto-install" "my-lisp")
 
 
