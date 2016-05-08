ee3a2209661350cfafa26a7c85d854d26ea375bd
Shunsuke KITADA <septemtrio.ager@gmail.com>
2016-05-08 11:27:05 +0900

    設定を追加

--- inits/77_markdown-mode.el
+++ inits/77_markdown-mode.el
@@ -12,11 +12,22 @@
 (el-get-bundle markdown-mode)
 
 (use-package markdown-mode
-  
+  :init
+  (add-hook 'markdown-mode-hook
+	    '(lambda ()
+	       (electric-indent-local-mode -1)
+	       (markdown-custom)))
+  :config
+  ;; ファイルパスを渡す
+  (defun markdown-custom ()
+    "markdown mode hook"
+    (setq markdown-command-needs-filename t)))
+
+(use-package gfm-mode
   :mode (("\\.text\\'" . gfm-mode)
 	 ("\\.markdown\\'" . gfm-mode)
 	 ("\\.md\\'" . gfm-mode))
-  
-  :config
-  ;; ファイルパスを渡す
-  (setq markdown-command-needs-filename t))
+  :init
+  (add-hook 'gfm-mode-hook
+	    '(lambda ()
+	       (electric-indent-local-mode -1))))
