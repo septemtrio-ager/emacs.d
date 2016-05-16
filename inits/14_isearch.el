76617697926e719d9383ffe02155e254b99bb579
Shunsuke KITADA <septemtrio.ager@gmail.com>
2016-05-16 11:03:50 +0900

    isearchの設定を分割

---
+++ inits/14_isearch.el
@@ -0,0 +1,5 @@
+;;
+;;; isearch.elの設定
+;;
+
+(define-key isearch-mode-map (kbd "C-h") 'isearch-del-char)
--- inits/35_ace-isearch.el
+++ inits/35_ace-isearch.el
@@ -5,25 +5,21 @@
 (el-get-bundle ace-isearch)
 
 (use-package ace-isearch
-  :defer t
+  ;; :commands isearch-forward-or-helm-swoop
   
-  :commands isearch-forward-or-helm-swoop 
   
-  :bind ("C-s" . isearch-forward-or-helm-swoop)
-  
-  :init
-  (global-ace-isearch-mode 1)
-  
-  (bind-key "C-h" 'isearch-del-char isearch-mode-map)
+  ;; :bind ("C-s" . isearch-forward-or-helm-swoop)
     
   :config
+  (global-ace-isearch-mode 1)
   ;; isearch中に一定文字以上になったらhelm-swoopを呼び出す
-  (defun isearch-forward-or-helm-swoop (use-helm-swoop)
-    (interactive "p")
-    (let (current-prefix-arg
-	  (helm-swoop-pre-input-function 'ignore))
-      (call-interactively
-       (case use-helm-swoop
-	 (1 'isearch-forward)
-	 (4 'helm-swoop)
-	 (16 'helm-swoop-nomigemo))))))
+  ;; (defun isearch-forward-or-helm-swoop (use-helm-swoop)
+  ;;   (interactive "p")
+  ;;   (let (current-prefix-arg
+  ;; 	  (helm-swoop-pre-input-function 'ignore))
+  ;;     (call-interactively
+  ;;      (case use-helm-swoop
+  ;; 	 (1 'isearch-forward)
+  ;; 	 (4 'helm-swoop)
+  ;; 	 (16 'helm-swoop-nomigemo)))))
+  )
