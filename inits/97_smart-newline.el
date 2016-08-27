839bc7038f1423456cd7d754f6a8d02597260017
Shunsuke KITADA <septemtrio.ager@gmail.com>
2016-08-28 01:24:15 +0900

    add smart-newline setting

---
+++ inits/97_smart-newline.el
@@ -0,0 +1,37 @@
+;;
+;; setting for smart-newline.el
+;; https://github.com/ainame/smart-newline.el
+
+;; ===================================================================
+
+;; 【参考】smart-newline.elという拡張
+;; http://ainame.hateblo.jp/entry/2013/12/08/162032
+
+;; ===================================================================
+
+;; keybind.el にて
+;;    (global-set-key (kbd "RET") 'smart-newline)
+;; として設定してある
+;; 個々には設定が効かないモードについて個別にhookさせる
+
+(el-get-bundle smart-newline)
+(use-package smart-newline
+  :init
+  ;; 個別にモードごとに設定をする
+  (dolist
+      (hook
+       '(
+
+	 ;; ess-modeで利用するようにする
+	 ess-mode-hook
+
+	 ;; markdown-modeで利用する
+	 markdown-mode-hook
+	 
+	 ;; gfm-modeで利用するようにする
+	 gfm-mode-hook
+	 
+	 ))
+    (add-hook hook
+	      (lambda ()
+		(smart-newline-mode 1)))))
