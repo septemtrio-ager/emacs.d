3efa836a25fff115aa425c6f0f7ba2b1df0f9e24
Shunsuke KITADA <septemtrio.ager@gmail.com>
2016-08-28 01:12:46 +0900

    don't compile after edited

--- init.el
+++ init.el
@@ -57,15 +57,15 @@
 
   ;; inits フォルダのみ, 保存時に自動コンパイルして即反映させる
   ;; http://fukuyama.co/emacsd
-  (defun auto-save-byte-compile-file ()
-    "Do `byte-compile-file' and reload setting immediately, When elisp file saved only in inits folder."
-    (interactive)
-    (when (or (equal default-directory inits_dir)
-	      (equal default-directory (abbreviate-file-name inits_dir)))
-      (byte-compile-file buffer-file-name t)))
+  ;; (defun auto-save-byte-compile-file ()
+  ;;   "Do `byte-compile-file' and reload setting immediately, When elisp file saved only in inits folder."
+  ;;   (interactive)
+  ;;   (when (or (equal default-directory inits_dir)
+  ;; 	      (equal default-directory (abbreviate-file-name inits_dir)))
+  ;;     (byte-compile-file buffer-file-name t)))
 
-  (add-hook 'emacs-lisp-mode-hook
-	    (lambda ()
-	      (add-hook 'after-save-hook 'auto-save-byte-compile-file nil t)))
+  ;; (add-hook 'emacs-lisp-mode-hook
+  ;; 	    (lambda ()
+  ;; 	      (add-hook 'after-save-hook 'auto-save-byte-compile-file nil t)))
   
   )
