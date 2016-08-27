fce8d57557a9b7b2425dc01b5866b1e2e40dde77
Shunsuke KITADA <septemtrio.ager@gmail.com>
2016-08-28 02:44:34 +0900

    add tramp setting

---
+++ inits/12_tramp.el
@@ -0,0 +1,16 @@
+;;
+;; setting for tramp.el
+;;
+
+;; ===================================================================
+
+;; 【参考】trampでリモートサーバのファイルをSSH経由で編集
+;; http://qiita.com/muyuu/items/63a82c819bbbe65d37a2
+
+;; ===================================================================
+
+(use-package tramp
+  ;; :defer t
+  :config
+  (setq tramp-default-method "ssh")
+  )
