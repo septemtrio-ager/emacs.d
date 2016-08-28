d8acb4bd232065f0a766c7660f6ccd6f8e8bc805
Shunsuke KITADA <septemtrio.ager@gmail.com>
2016-08-28 18:44:23 +0900

    add highlight-symbol setting

---
+++ inits/36_highlight-symbol.el
@@ -0,0 +1,44 @@
+;;
+;; setting for highlight-symbol.el
+;; https://github.com/nschum/highlight-symbol.el
+
+;; ===================================================================
+
+;; 【参考】シンボルを自動・手動ハイライト、移動、置換、マッチ数表示
+;; http://rubikitch.com/2015/10/07/highlight-symbol/
+
+;; ===================================================================
+
+(el-get-bundle highlight-symbol)
+
+(use-package highlight-symbol
+  :defer t
+  
+  :init
+  ;; 自動ハイライトするmodeを設定
+  (dolist (hook '(
+		  
+		  ;; プログラミング言語modeに適応
+		  prog-mode-hook
+		  
+		  nxml-mode-hook
+		  
+		  ))
+    (add-hook hook 'highlight-symbol-mode))
+  
+  ;; ソースコードにおいてM-p/M-nでシンボル間を移動できるようにする
+  (dolist (hook '(
+		  
+		  ;; プログラミング言語modeに適応
+		  prog-mode-hook
+		  
+		  nxml-mode-hook
+		  
+		  ))
+    (add-hook hook 'highlight-symbol-nav-mode))
+  
+  :bind ("C-c r" . highlight-symbol-query-replace) ;; シンボル置換できるようにする
+  
+  :config
+  ;; 1秒後自動ハイライトされるようになる
+  (setq highlight-symbol-idle-delay 1.0))
