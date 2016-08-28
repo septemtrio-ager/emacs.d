7a048d3702bcf959fbfa13997f9e7c7eb7badadf
Shunsuke KITADA <septemtrio.ager@gmail.com>
2016-08-28 22:30:29 +0900

    add rainbow-mode setting

---
+++ inits/97_rainbow-mode.el
@@ -0,0 +1,38 @@
+;;
+;; setting for rainbow-mode.el
+;; https://github.com/emacsmirror/rainbow-mode
+
+;; ===================================================================
+
+;; 【参考】Emacsで #f95327 などの文字列に色を付けておく
+;; http://qiita.com/ironsand/items/cf8c582da3ec20715677
+
+;; ===================================================================
+
+(el-get-bundle rainbow-mode)
+
+(use-package rainbow-mode
+  :defer t
+  :init
+  (dolist (hook '(
+		  
+		  ;; css-modeで利用する
+		  css-mode-hook
+
+		  ;;scss-modeで利用する
+		  scss-mode-hook
+
+		  ;; php-modeで利用する
+		  php-mode-hook
+
+		  ;; html-modeで利用する
+		  html-mode-hook
+
+		  ;; emacs-lisp-modeで利用する
+		  emacs-lisp-mode-hook
+
+		  ;; nxml-modeで利用する
+		  nxml-mode-hook
+		  
+		  ))
+    (add-hook hook 'rainbow-mode)))
