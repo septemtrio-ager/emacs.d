82d8b88c12ff4b0658ecfcd29930921fdab3bb15
Shunsuke KITADA <septemtrio.ager@gmail.com>
2016-08-28 18:44:39 +0900

    add centered-cursor-mode setting

---
+++ inits/37_centered-cursor-mode.el
@@ -0,0 +1,17 @@
+;;
+;; setting for centered-cursor-mode.el
+;; https://www.emacswiki.org/emacs/centered-cursor-mode.el
+
+;; ===================================================================
+
+;; 【参考】カーソル移動・スクロール時に常に画面中央行(任意可)を
+;;                                            キープして表示させる
+;; http://rubikitch.com/2015/04/23/centered-cursor-mode/
+
+;; ===================================================================
+
+(el-get-bundle centered-cursor-mode)
+
+(use-package centered-cursor-mode
+  :defer t)
+
