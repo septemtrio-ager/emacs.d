08fc35b2425ff8347737d187d4ccf68d9f873efa
Shunsuke KITADA <septemtrio.ager@gmail.com>
2016-08-28 18:32:33 +0900

    add eval-in-repl setting

---
+++ inits/32_eval-in-repl.el
@@ -0,0 +1,19 @@
+;;
+;; setting for eval-repl.el
+;; https://github.com/shunk031/eval-in-repl
+
+;; ===================================================================
+
+;; 【参考】ESS+R使いがClojureで遊びたくて作ったeval-in-repl.el 
+;; http://qiita.com/kaz-yos/items/3cf663d67f69d3cc2fc2
+
+;; ===================================================================
+
+(el-get-bundle shunk031/eval-in-repl)
+
+(use-package eval-in-repl-python
+  :commands eir-eval-in-python)
+
+(add-hook 'python-mode-hook
+          '(lambda ()
+             (local-set-key (kbd "<C-return>") 'eir-eval-in-python)))
