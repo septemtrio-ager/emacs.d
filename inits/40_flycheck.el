c3f76ecc0f01289b681f34650dd511d15b680a8c
Shunsuke KITADA <septemtrio.ager@gmail.com>
2016-08-28 18:58:02 +0900

    add flycheck setting

---
+++ inits/40_flycheck.el
@@ -0,0 +1,28 @@
+;;
+;;; flycheck.elの設定
+;;
+
+;; ===================================================================
+
+;; 【参考】Flycheckでモダンなシンタックスチェック
+;; http://qiita.com/senda-akiha/items/cddb02cfdbc0c8c7bc2b
+
+;; ===================================================================
+
+(el-get-bundle flycheck)
+
+(use-package flycheck
+  :defer t
+  :init
+  (add-hook 'after-init-hook #'global-flycheck-mode)
+  
+  :config
+
+  ;; =================================================================
+
+  ;; 【参考】flycheck — how to disable warnning while edit emacs lisp scripts?
+  ;; http://stackoverflow.com/questions/15552349/flycheck-how-to-disable-warnning-while-edit-emacs-lisp-scripts
+
+  ;; =================================================================
+
+  (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
