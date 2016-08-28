61fa4fd2a7ea4256774c7152ec3d5c13de4e1625
Shunsuke KITADA <septemtrio.ager@gmail.com>
2016-08-28 21:01:07 +0900

    add highlight-indentation setting

---
+++ inits/44_highlight-indentation.el
@@ -0,0 +1,10 @@
+;;
+;; setting for highlight-indentation.el
+;; https://github.com/antonj/Highlight-Indentation-for-Emacs
+
+(use-package highlight-indentation
+  :defer t
+  :config
+  ;; (set-face-background 'highlight-indentation-face "gray5")
+  
+  )
