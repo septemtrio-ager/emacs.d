5f1f8b96c393513407532a2e95da8a3630ac527c
Shunsuke KITADA <septemtrio.ager@gmail.com>
2017-05-22 23:40:04 +0900

    add import-popwin settings

---
+++ inits/05_import-popwin.el
@@ -0,0 +1,9 @@
+;;
+;; setting for import-popwin.el
+;; https://github.com/syohex/emacs-import-popwin
+
+(el-get-bundle import-popwin)
+
+(use-package import-popwin
+  :bind (("M-g M-i" . import-popwin))
+  )
